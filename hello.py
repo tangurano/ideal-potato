52502c57f55979878352b34c9b27af42f9ad3ea5
Charles Tang <charles.tang@utexas.edu>
2016-06-09 20:37:03 -0700

    Person class

--- hello.py
+++ hello.py
@@ -1,6 +1,23 @@
 
-def introduce(name):
-  print "Hello everyone, this is ", name
+class Person:
+
+  def __init__(self, name):
+  	self.name = name
+
+  def __str__(self):
+  	return self.name
+
+def introduce(p):
+  # print adds an extra space?
+  print "Hello everyone, this is", p
 
 
-print introduce("Charles")
+#print introduce("Charles")
+#print introduce(Person("Charles"))
+
+names = ["Arya", "Sansa", "Ned"]
+contacts = [Person(name) for name in names]
+#printing array doesn't use __str__ on elements?
+# print contacts
+for c in contacts:
+  print c
